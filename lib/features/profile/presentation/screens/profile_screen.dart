6ef077814f7d67c483a5e73deed64cc6718c92ba
YoussefHanna64-bit <JCBIOX200@GMAIL.COM>
2026-06-25 18:25:05 +0300

    Feat: add TabBar and integrate it in ProfileScreen

--- lib/features/profile/presentation/screens/profile_screen.dart
+++ lib/features/profile/presentation/screens/profile_screen.dart
@@ -12,28 +12,65 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFF212121),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SingleChildScrollView(
-              child: Column(
-            children: [
-              ProfileHeader(
-                username: "Legend",
-                avatarUrl:
-                    "https://avatarfiles.alphacoders.com/823/thumb-1920-82313.jpg",
-                wishlistCount: 12,
-                watchedCount: 10,
-              ),
-              const SizedBox(height: 24),
-              ProfileActions(
-                onEditPressed: () {},
-                onLogoutPressed: () {},
-              ),
-            ],
-          )),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        backgroundColor: const Color(0xFF212121),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                ProfileHeader(
+                  username: "Legend",
+                  avatarUrl:
+                      "https://avatarfiles.alphacoders.com/823/thumb-1920-82313.jpg",
+                  wishlistCount: 12,
+                  watchedCount: 10,
+                ),
+                const SizedBox(height: 16),
+                ProfileActions(
+                  onEditPressed: () {},
+                  onLogoutPressed: () {},
+                ),
+                const SizedBox(height: 4),
+                const TabBar(
+                  dividerColor: Colors.transparent,
+                  indicatorColor: Color(0xFFF6BD00),
+                  labelColor: Color(0xFFF6BD00),
+                  unselectedLabelColor: Colors.grey,
+                  tabs: [
+                    Tab(
+                      icon: Icon(Icons.list, size: 30),
+                      text: "Watch List",
+                    ),
+                    Tab(
+                      icon: Icon(Icons.history, size: 30),
+                      text: "History",
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: TabBarView(
+                    children: [
+                      Center(
+                        child: Text(
+                          "Watch List",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      Center(
+                        child: Text(
+                          "History",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
